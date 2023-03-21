7932c7acafe05ccf2082c763ac05d96964449fe3
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-03-21 11:16:41 -0300

    Mensagens do sistema

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -7,18 +7,20 @@ function Select({text, name, options, handleOnChange, value}) {
         htmlFor={name}>
         {text}
       </label>
-      <select name={name} id={name}>
-        {
-          options.map((item) => {
-            return (
-              <option 
-                value={item.id} 
-                key={item.id}>
-                {item.name}
-              </option>
-            )
-          })
-        }
+      <select 
+        name={name} 
+        id={name} 
+        onChange={handleOnChange} 
+        value={value || ''}
+      >
+        <option>Selecione uma opção</option>
+        {options.map((item) => (
+          <option 
+            value={item.id} 
+            key={item.id}>
+            {item.name}
+          </option>
+        ))}
       </select>
     </div>
   )
