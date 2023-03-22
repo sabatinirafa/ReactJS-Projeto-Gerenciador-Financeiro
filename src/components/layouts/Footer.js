78943918465d59da52c95a9376dd27d0c91e6cef
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-03-22 00:05:22 -0300

    Projects page

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
   return(
     <footer className={styles.footer}>
       <ul className={styles.social_list}>
-        <li><FaGithub/></li>
-        <li><FaLinkedin/></li>
-        <li><MdEmail/></li>
+        <li><a href='https://github.com/sabatinirafa' target='_blank'><FaGithub/></a></li>
+        <li><a href='https://www.linkedin.com/in/rafael-sabatini-41819a1b2/' target='_blank'><FaLinkedin/></a></li>
+        <li><a href='mailto:rafa.sabatini@yahoo.com.br' ><MdEmail/></a></li>
       </ul>
       <p className={styles.copy_right}><span>Costs</span> &copy; 2023</p>
     </footer>
