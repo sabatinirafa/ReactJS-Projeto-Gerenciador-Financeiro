453482130e5bdd327351207fc9c7ab88833efebb
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-03-24 11:34:22 -0300

    Atualização de projetos

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,6 +3,7 @@ import styles from './Project.module.css'
 import Loading from '../layouts/Loading'
 import Container from '../layouts/Container'
 import ProjectForm from '../project/ProjectForm'
+import Message from '../layouts/Message'
 
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
@@ -12,6 +13,9 @@ function Project() {
   const { id } = useParams()
   const [project, setProject] = useState([])
   const [showProjectForm, setShowProjectForm] =useState(false)
+  const [message, setMessage] =useState()
+  const [type, setType] =useState()
+
 
   useEffect(() => {
 
@@ -32,8 +36,30 @@ function Project() {
     setShowProjectForm(!showProjectForm)
   }
 
-  function editPost() {
-    
+  function editPost(project) {
+    // budget validation, custo total nao pode ser menor que zero
+    if(project.budget < project.cost) {
+      setMessage('O orçamento não pode ser menor que o custo do projeto')
+      setType('error')
+      return false
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      setProject(data)
+      setShowProjectForm(false)
+      setMessage('Projeto atualizado')
+      setType('success')
+    })
+    .catch(err => console.log(err))
+
   }
 
   return(
@@ -41,6 +67,7 @@ function Project() {
       {project.name ? (
         <div className={styles.project_details}>
           <Container customClass='column'>
+            {message && <Message type={type} msg={message}/>}
             <div className={styles.details_container}>
               <h1>Projeto: {project.name}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
